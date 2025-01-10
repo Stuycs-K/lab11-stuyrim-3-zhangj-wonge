cf653bf2fbd56765ca602278639e1ecd64d467b0
Eunice Wong <eunicew4@nycstudents.net>
2025-01-09 21:25:06 -0500

    fixed methods to fit Rogue

--- Rogue.java
+++ Rogue.java
@@ -1,67 +1,66 @@
-public class CodeWarrior extends Adventurer{
-  int caffeine, caffeineMax;
+public class Rogue extends Adventurer{
+  int Apathy, ApathyMax;
   String preferredLanguage;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
-  public CodeWarrior(String name, int hp, String language){
+  public Rogue(String name, int hp, ){
     super(name,hp);
-    caffeineMax = 12;
-    caffeine = caffeineMax/2;
-    preferredLanguage = language;
+    ApathyMax = 5;
+    Apathy = 0;
   }
 
-  public CodeWarrior(String name, int hp){
-    this(name,hp,"c++");
+  public Rogue(String name, int hp){
+    this(name,hp);
   }
 
-  public CodeWarrior(String name){
-    this(name,24);
+  public Rogue(String name){
+    this(name,75);
   }
 
-  public CodeWarrior(){
+  public Rogue(){
     this("Carmack");
   }
 
   /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
-    return "caffeine";
+    return "Apathy";
   }
 
   public int getSpecial(){
-    return caffeine;
+    return Apathy;
   }
 
   public void setSpecial(int n){
-    caffeine = n;
+    Apathy = n;
   }
 
   public int getSpecialMax(){
-    return caffeineMax;
+    return ApathyMax;
   }
 
-  /*Deal 2-7 damage to opponent, restores 2 caffeine*/
+  /*Deal 2-7 damage to opponent, restores 2 Apathy*/
   public String attack(Adventurer other){
-    int damage = (int)(Math.random()*6)+2;
+    int damage = (int)(Math.random()*6)+10;
     other.applyDamage(damage);
-    restoreSpecial(2);
+    Apathy += 1;
+    
     return this + " attacked "+ other + " and dealt "+ damage +
-    " points of damage. They then take a sip of their coffee.";
+    " points of damage.";
   }
 
-  /*Deal 3-12 damage to opponent, only if caffeine is high enough.
-  *Reduces caffeine by 8.
+  /*Deal 3-12 damage to opponent, only if Apathy is high enough.
+  *Reduces Apathy by 8.
   */
   public String specialAttack(Adventurer other){
-    if(getSpecial() >= 8){
-      setSpecial(getSpecial()-8);
-      int damage = (int)(Math.random()*5+Math.random()*5)+3;
+    if(getSpecial() >= 5){
+      setSpecial(getSpecial()-5);
+      int damage = 75;
       other.applyDamage(damage);
-      return this + " used their "+preferredLanguage+
-      " skills to hack the matrix. "+
-      " This glitched out "+other+" dealing "+ damage +" points of damage.";
+      return this + " hit a critical spot with their dagger. "+
+      " This glitched out "+other+", dealing "+ damage +" points of damage.";
     }else{
-      return "Not enough caffeine to use the ultimate code. Instead "+attack(other);
+      return "Not enough Apathy to use the ultimate code. Instead "+attack(other);
     }
 
   }
