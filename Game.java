a70d33dab2c572fcefdba566b9b4f12b52dd3212
Jeffery Zhang <jzhang60@stuy.edu>
2025-01-10 10:09:16 -0500

    Implemented TextBox Method

--- Game.java
+++ Game.java
@@ -63,9 +63,6 @@ public class Game{
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
   }
 
-
-
-
     //return a random adventurer (choose between all available subclasses)
     //feel free to overload this method to allow specific names/stats.
     public static Adventurer createRandomAdventurer(){
